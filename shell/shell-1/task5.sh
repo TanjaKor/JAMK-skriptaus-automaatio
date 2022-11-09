25a8ad88b129dee3e6fa3eb7504520ff21e8a207
AB3775 <ab3775@student.jamk.fi>
2022-11-09 19:42:18 +0200

    korjattu kirjoitusvirhe

--- shell/shell-1/task5.sh
+++ shell/shell-1/task5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #luodaan kotikansioon polku, joka voidaan yhdistää myöhemmin muuttujiin
-polku=/home/user/
+polku="/home/user/"
 #poistetaan tiedostot
 rm "${polku}$1" "${polku}$2" "${polku}$3"
 #tulostetaan kuittaus
